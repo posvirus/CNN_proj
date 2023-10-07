0e2fb9bd81759794222aa65235e4955d7b7f6645
Mee <129379106+Mee-6605@users.noreply.github.com>
2023-10-08 00:39:56 +0800

    Add _I/O_ definition to avoid bug during ICC

--- top/cnn_top_chip.v
+++ top/cnn_top_chip.v
@@ -22,22 +22,22 @@ wire net_scan_out, net_phi_out, net_clk_div1024_out, net_phib_out, net_scan_i0o1
 
 
 HPDWUW1416DGP
-    HPDWUW1416DGP_clk_signal_ext(.PAD(clk_signal_ext),.C(net_clk_signal_ext),.IE(1'b1)),
-    HPDWUW1416DGP_rst_n(.PAD(rst_n),.C(net_rst_n),.IE(1'b1)),
-    HPDWUW1416DGP_write_en(.PAD(write_en),.C(net_write_en),.IE(1'b1)),
-    HPDWUW1416DGP_phi(.PAD(phi),.C(net_phi),.IE(1'b1)),
-    HPDWUW1416DGP_phib(.PAD(phib),.C(net_phib),.IE(1'b1)),
-    HPDWUW1416DGP_scan_i0o1(.PAD(scan_i0o1),.C(net_scan_i0o1),.IE(1'b1)),
-    HPDWUW1416DGP_load(.PAD(load),.C(net_load),.IE(1'b1)),
-    HPDWUW1416DGP_scan_in(.PAD(scan_in),.C(net_scan_in),.IE(1'b1));
+    HPDWUW1416DGP_I_clk_signal_ext(.PAD(clk_signal_ext),.C(net_clk_signal_ext),.IE(1'b1)),
+    HPDWUW1416DGP_I_rst_n(.PAD(rst_n),.C(net_rst_n),.IE(1'b1)),
+    HPDWUW1416DGP_I_write_en(.PAD(write_en),.C(net_write_en),.IE(1'b1)),
+    HPDWUW1416DGP_I_phi(.PAD(phi),.C(net_phi),.IE(1'b1)),
+    HPDWUW1416DGP_I_phib(.PAD(phib),.C(net_phib),.IE(1'b1)),
+    HPDWUW1416DGP_I_scan_i0o1(.PAD(scan_i0o1),.C(net_scan_i0o1),.IE(1'b1)),
+    HPDWUW1416DGP_I_load(.PAD(load),.C(net_load),.IE(1'b1)),
+    HPDWUW1416DGP_I_scan_in(.PAD(scan_in),.C(net_scan_in),.IE(1'b1));
    
 HPDWUW1416DGP
-    HPDWUW1416DGP_scan_out(.PAD(scan_out),.I(net_scan_out),.OE(1'b1)),
-    HPDWUW1416DGP_phi_out(.PAD(phi_out),.I(net_phi_out),.OE(1'b1)),
-    HPDWUW1416DGP_clk_div1024_out(.PAD(clk_div1024_out),.I(net_clk_div1024_out),.OE(1'b1)),
-    HPDWUW1416DGP_phib_out(.PAD(phib_out),.I(net_phib_out),.OE(1'b1)),
-    HPDWUW1416DGP_scan_i0o1_out(.PAD(scan_i0o1_out),.I(net_scan_i0o1_out),.OE(1'b1)),
-    HPDWUW1416DGP_load_out(.PAD(load_out),.I(net_load_out),.OE(1'b1));
+    HPDWUW1416DGP_O_scan_out(.PAD(scan_out),.I(net_scan_out),.OE(1'b1)),
+    HPDWUW1416DGP_O_phi_out(.PAD(phi_out),.I(net_phi_out),.OE(1'b1)),
+    HPDWUW1416DGP_O_clk_div1024_out(.PAD(clk_div1024_out),.I(net_clk_div1024_out),.OE(1'b1)),
+    HPDWUW1416DGP_O_phib_out(.PAD(phib_out),.I(net_phib_out),.OE(1'b1)),
+    HPDWUW1416DGP_O_scan_i0o1_out(.PAD(scan_i0o1_out),.I(net_scan_i0o1_out),.OE(1'b1)),
+    HPDWUW1416DGP_O_load_out(.PAD(load_out),.I(net_load_out),.OE(1'b1));
 	
 
 
